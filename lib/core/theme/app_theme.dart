2233546c0d23063b9e7bd6e1310baefff8e453af
heojeongbo <heojungbo@gmail.com>
2021-08-04 01:04:07 +0900

    getX 적용해 기본 구조 작성

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final ThemeData appThemeData = ThemeData(
+  primaryColor: Colors.black,
+  textTheme: GoogleFonts.robotoTextTheme(),
+);
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+part './app_pages.dart';
+
+abstract class Routes {
+  static const INITIAL = '/';
+  static const SIGNUP = '/SIGN_UP';
+  static const FORGET_PASSWORD = '/FORGET_PASSWORD';
+}
